72c8c16295ed51b01dae7e837c9eece4e092edec
superdev <newestStar1990@gmail.com>
2024-04-18 16:31:56 -0400

    123

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatBadgeModule } from "@angular/material/badge";
 import { PeopleListItemComponent } from "../../components/peoplelistitem/peoplelistitem.component";
 import { PeopleScreenComponent } from "../../components/peoplescreen/peoplescreen.component";
+import { RemoteParticipant, RemoteTrack, RemoteTrackPublication, Room, RoomEvent, VideoPresets, createLocalAudioTrack, createLocalVideoTrack } from "livekit-client";
 
 @Component({
 	selector: "app-dashboard",
@@ -18,6 +19,62 @@ export class DashboardComponent implements AfterViewInit {
 	sideTabVisible = false;
 	meeting_title = "Business Weekly Meeting";
 	meeting_time = "April 16th, 2023 | 10:00 AM";
+
+	@ViewChild("mainView") mainViewRef: ElementRef | null = null;
+	@ViewChild("screenContainer") screenContainerRef: ElementRef | null = null;
+
+	ngAfterViewInit(): void {
+		this.fitScreenPos();
+	}
+
+	@HostListener("window:resize", ["$event"])
+	onResize(event: any) {
+		this.fitScreenPos();
+	}
+
+	async joinRoom() {
+		const wsUrl = "wss://hotelgenie-ae73y1rz.livekit.cloud";
+		const resToken = await fetch("http://localhost:3000/getToken");
+		const token = await resToken.text();
+		const room = new Room();
+
+		await room.connect(wsUrl, token);
+		await room.localParticipant.enableCameraAndMicrophone();
+
+		room.localParticipant.setCameraEnabled(true);
+		room.localParticipant.setMicrophoneEnabled(true);
+
+		// await room.localParticipant.setScreenShareEnabled(true);
+
+		const videoTrack = await createLocalVideoTrack({
+			facingMode: "user",
+			resolution: VideoPresets.h720,
+		});
+		const audioTrack = await createLocalAudioTrack({
+			echoCancellation: true,
+			noiseSuppression: true,
+		});
+
+		const element = videoTrack.attach();
+		element.setAttribute("width", "100%");
+		element.setAttribute("height", "100%");
+		this.mainViewRef?.nativeElement.appendChild(element);
+
+		// const videoPublication = await room.localParticipant.publishTrack(videoTrack);
+		// const audioPublication = await room.localParticipant.publishTrack(audioTrack);
+
+		room.on(RoomEvent.TrackSubscribed, this.handleTrackSubscribed);
+	}
+
+	handleTrackSubscribed(track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) {
+		console.log(participant);
+		const element = track.attach();
+		element.setAttribute("width", "160px");
+		element.setAttribute("height", "90px");
+		// document.body.appendChild(element);
+
+		document.getElementsByClassName("hg-screen-container")[0].appendChild(element);
+	}
 
 	toggleMic(event: any, self: any) {
 		console.log(this);
@@ -48,6 +105,7 @@ export class DashboardComponent implements AfterViewInit {
 	}
 
 	fitScreenPos() {
+		console.log(this.screenContainerRef?.nativeElement);
 		const width = this.mainViewRef?.nativeElement.parentElement.offsetWidth;
 		const height = this.mainViewRef?.nativeElement.parentElement.offsetHeight - this.screenContainerRef?.nativeElement.offsetHeight - 80;
 		if (width > Math.round((height * 16) / 9)) {
@@ -57,17 +115,5 @@ export class DashboardComponent implements AfterViewInit {
 			this.mainViewRef?.nativeElement.style.setProperty("width", "100%");
 			this.mainViewRef?.nativeElement.style.setProperty("height", "auto");
 		}
-	}
-
-	@ViewChild("mainView") mainViewRef: ElementRef | null = null;
-	@ViewChild("screenContainer") screenContainerRef: ElementRef | null = null;
-
-	ngAfterViewInit(): void {
-		this.fitScreenPos();
-	}
-
-	@HostListener("window:resize", ["$event"])
-	onResize(event: any) {
-		this.fitScreenPos();
 	}
 }
